b37c681977cef12abd6109b17156d23ca4136072
gcavusogluKU <gcavusoglu19@ku.edu.tr>
2025-01-11 15:09:58 +0300

    Add Lane-Emden equation solver

--- part_c.py
+++ part_c.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.integrate import solve_ivp
 
 def fit_WD_data(M,R,minR=0.15):
     def M_fit(R, A, n): #Curve_fit fitting function
@@ -17,3 +18,21 @@ def get_int_q(n):
     q = 5*n/(1+n)       #Get q from n
     q = np.rint(q)      #Set q to nearest integer
     return q
+
+def lane_emden_system(ξ, y, n=1.5): #Coupled system of ODEs, dθ/dξ = u, du/dξ = -2u/ξ-θ^(3/2)
+    θ, u = y
+    if ξ == 0: # Avoid division by zero at the center (ξ=0)
+        dθ_dξ = 0
+        du_dξ = 2/3 - θ**n
+    else:
+        dθ_dξ = u   # dθ/dξ = u
+        du_dξ = -2 / ξ * u - θ**n # du/dξ = -2u/ξ-θ^(3/2)
+    return [dθ_dξ, du_dξ]
+
+def solve_lane_emden(method="RK45", ξmax=5, max_step=1e-3, jac=None, atol=1e-6, rtol=1e-3):
+    θ0 = (1,0) #Since θ(1) = 1 and θ'(0) = 0
+    if jac == None:
+        solution = solve_ivp(lane_emden_system, [0, ξmax], θ0, method=method, max_step=1e-4, atol=atol, rtol=rtol)
+    else:
+        solution = solve_ivp(lane_emden_system, [0, ξmax], θ0, method=method, max_step=1e-4, atol=atol, rtol=rtol, jac=jac)
+    return solution
